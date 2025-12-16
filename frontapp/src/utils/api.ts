c3a93c7a92a45ae0adf507e47344fc47ed0ff6a0
kimhyeji <gpwl5589@gmail.com>
2025-12-16 23:34:58 +0900

    work

--- frontapp/src/utils/api.ts
+++ frontapp/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8090/api/v1/',
+  baseURL: 'https://api.hj-yee.shop/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
