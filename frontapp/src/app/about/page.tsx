c32f78f73fb404db09be2662489aecb7de201acb
kimhyeji <gpwl5589@gmail.com>
2025-12-16 22:59:32 +0900

    work

--- frontapp/src/app/about/page.tsx
+++ frontapp/src/app/about/page.tsx
@@ -3,20 +3,30 @@
 import { useEffect, useState } from 'react'
 import api from '../../utils/api'
 
+type Member = {
+  id: number
+  username: string
+}
+
 export default function About() {
-  const [member, setMember] = useState({})
+  const [member, setMember] = useState<Member | null>(null)
 
   useEffect(() => {
-    api.get('/members/me').then((res) => setMember(res.data.data.memberDto))
+    api.get('/members/me').then((res) => {
+      setMember(res.data.data.memberDto)
+    })
   }, [])
 
   return (
     <>
       <h4>소개 페이지</h4>
-      <ul>
-        <li>id : {member.id}</li>
-        <li>username : {member.username}</li>
-      </ul>
+
+      {member && (
+        <ul>
+          <li>id : {member.id}</li>
+          <li>username : {member.username}</li>
+        </ul>
+      )}
     </>
   )
 }
